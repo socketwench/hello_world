8a5a17bb0e6ba5c870e0ad31b99b87d866c8c108
socketwench <tess@deninet.com>
2016-07-20 10:03:02 -0500

    Display Hello World Article links in Hello block with tags About Us, Misc, or News.

--- src/Plugin/Block/HelloBlock.php
+++ src/Plugin/Block/HelloBlock.php
@@ -6,6 +6,7 @@ use Drupal\Core\Block\BlockBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\Query\Sql\QueryFactory;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
 
 /**
  * Provides a 'HelloBlock' block.
@@ -27,28 +28,60 @@ class HelloBlock extends BlockBase {
   /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, QueryFactory $entity_query) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) { //}, QueryFactory $entity_query) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity.query')
-    );
-  }
+//  /**
+//   * {@inheritdoc}
+//   */
+//  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+//    return new static(
+//      $configuration,
+//      $plugin_id,
+//      $plugin_definition,
+//      $container->get('entity.query')
+//    );
+//  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
     $build = [];
-    $build['hello_block']['#markup'] = 'Implement HelloBlock.';
+
+    // Query for published Hello World Articles with the given tags.
+    $entity_query = \Drupal::entityQuery('node')
+      ->condition('status', '1')
+      ->condition('type', 'hello_world_article')
+      ->condition('field_sections.entity.name', ['About Us', 'Misc', 'News'], 'IN');
+
+    $result = $entity_query->execute();
+
+    if (empty($result)) {
+      // Display an empty message if there are no results.
+      $build['hello_block']['#markup'] = t('There are no Hello articles to display.');
+    }
+    else {
+      // Use Core's links template, but allow easy override.
+      $build['hello_block'] = [
+        '#theme' => 'links__hello',
+        '#attributes' => [
+          'class' => [],
+        ],
+      ];
+
+      // Load all the nodes at once.
+      $nodes = Node::loadMultiple($result);
+
+      foreach ($nodes as $node) {
+        // Add to links array.
+        $build['hello_block']['#links'][] = [
+          'title' => $node->label(),
+          'url' => $node->urlInfo(),
+        ];
+      }
+    }
 
     return $build;
   }
